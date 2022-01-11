4e39ce2aa580b339bef4b641ba53da2915c1ef59
Mark de Jesus <markdejesus@Sherwins-MacBook-Air.local>
2022-01-11 19:57:43 +0800

    first commit

---
+++ app/Http/Controllers/CarsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CarsController extends Controller
+{
+    public function index(){
+        $brands = ['ford', 'toyota', 'honda', 'hyundai', 'mitsubishi', 'subaru', 'rolls royce'];
+        $cars = \App\Models\Car::all();
+        
+        return view('welcome', array('brands' => $brands, 'cars'=>$cars));
+    }
+
+    public function create(Request $form){
+        \App\Models\Car::create([
+            'brand' => $form->brand,
+            'model' => $form->model,
+            'year' => $form->year,
+            'color' => $form->color,
+            'plate' => $form->plate,
+        ]);
+        return redirect('/');
+    }
+    public function delete(Request $form){
+        \App\Models\Car::find($form->id)->delete();
+            return redirect ('/');
+    }
+    
+}
